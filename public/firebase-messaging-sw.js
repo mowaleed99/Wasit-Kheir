b927d6213b20043e165f21f3b3895ffbae0ed023
mowaleed99 <mohamedwaleed450@gmail.com>
2026-06-16 20:33:41 +0300

    Update logo to logo2.png across project

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -21,7 +21,7 @@ messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/logo.jpg'
+        icon: '/logo2.png'
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
